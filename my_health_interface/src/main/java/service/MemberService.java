f3f9335a71b439893acb3d53728b289a9bd15644
Lee <493216081@qq.com>
2019-08-04 10:45:09 +0800

    定时任务

--- my_health_interface/src/main/java/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import com.lwy.pojo.Member;
-
-public interface MemberService {
-    Member findByTelephone(String telephone);
-
-    void add(Member member);
-}
--- my_health_web/src/test/java/Test.java
+++
@@ -1,9 +0,0 @@
-
-import sun.security.provider.MD5;
-
-public class Test {
-    @org.junit.Test
-    public void test1(){
-
-    }
-}
